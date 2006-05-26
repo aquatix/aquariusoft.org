24690b1c16728f6fd29d8cd27b4827e6bf4df3c4
Michiel Scholten <michiel@aquariusoft.org>
2006-05-26 14:00:47 +0000

    v20060403b
    --HG--
    extra : convert_revision : svn%3A6d5ff042-b014-0410-9faa-87b5a50638ce/trunk%4017

--- config.php
+++ config.php
@@ -1,19 +1,27 @@
 <?php
 
-	/*
-	 * set the language to be used by the framework
-	 * you can choose between en for English and nl for Dutch
-	 */
-	$skel['language'] = 'en';
+/*
+ * set the language to be used by the framework
+ * you can choose between en for English and nl for Dutch
+ */
+$skel['language'] = 'en';
 
-	/* Additional information to be shown when a user gets a 'page not found' */
-	$skel['404_message'] = '<p>If you are looking for the overload feedreader, please <a href="https://overload.aquariusoft.org">look at overloads new address</a></p>';
-	$skel['404_message'] .= '<p><em>The gallery will be up soon again</em>; as fast as the old server arrives here and the files have been copied.</p>';
-	//$skel['404_message'] .= '<p>Also, the bugtracker, gallery, forum and webmail and the weblog dammIT will be online again soon.</p>';
+/* Additional information to be shown when a user gets a 'page not found' */
+$skel['404_message'] = '<p>If you are looking for the overload feedreader, please <a href="https://overload.aquariusoft.org">look at overloads new address</a></p>';
+$skel['404_message'] .= '<p><em>The gallery will be up soon again</em>; as fast as the old server arrives here and the files have been copied.</p>';
+//$skel['404_message'] .= '<p>Also, the bugtracker, gallery, forum and webmail and the weblog dammIT will be online again soon.</p>';
 
-	$skel['logfile'] = '/var/log/qik/aquariusoftorg.log';
-	$skel['logpass'] = 'hallo666';
+/* Set usedb to false for using flat files [only supported mechanism at the moment] */
+$skel['usedb'] = false;
+$skel['dbhost'] = '';
+$skel['dbname'] = '';
+$skel['dbuser'] = '';
+$skel['dbpass'] = '';
 
-	/* the text to be displayed in the footer */
-	$skel['copyright'] = '&copy; 1999-2006 webmaster (at) aquariusoft.org';
+$skel['logfile'] = '/var/log/qik/aquariusoftorg.log';
+$skel['logpass'] = 'hallo666';
+
+/* the text to be displayed in the footer */
+$skel['copyright'] = '&copy; 1999-2006 webmaster (at) aquariusoft.org';
+
 ?>
