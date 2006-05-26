b9c5746beaf581699e9d464a3b8c9ff51927c286
Michiel Scholten <michiel@aquariusoft.org>
2006-05-26 13:46:15 +0000

    v20060124
    --HG--
    extra : convert_revision : svn%3A6d5ff042-b014-0410-9faa-87b5a50638ce/trunk%409

--- config.php
+++ config.php
@@ -8,6 +8,7 @@
 
 	/* Additional information to be shown when a user gets a "page not found" */
 	$skel["404_message"] = "<p>If you are looking for the overload feedreader, please <a href=\"https://overload.aquariusoft.org\">look at overloads new address</a></p>";
+	//$skel["404_message"] .= "<p>Also, the bugtracker, gallery, forum and webmail and the weblog dammIT will be online again soon.</p>";
 
 	/* the text to be displayed in the footer */
 	$skel["copyright"] = "&copy; 1999-2006 webmaster (at) aquariusoft.org";
