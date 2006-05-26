4179297aea12c9aab13d7f778d369a425fa7efad
Michiel Scholten <michiel@aquariusoft.org>
2006-05-26 12:50:09 +0000

    v20060119c
    --HG--
    extra : convert_revision : svn%3A6d5ff042-b014-0410-9faa-87b5a50638ce/trunk%403

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+	/*
+	 * set the language to be used by the framework
+	 * you can choose between en for English and nl for Dutch
+	 */
+	$skel["language"] = "en";
+
+	/* Additional information to be shown when a user gets a "page not found" */
+	$skel["404_message"] = "<p>If you are looking for the overload feedreader, please <a href=\"https://overload.aquariusoft.org\">look at overloads new address</a></p>";
+
+	/* the text to be displayed in the footer */
+	$skel["copyright"] = "&copy; 1999-2006 webmaster (at) aquariusoft.org";
+?>
