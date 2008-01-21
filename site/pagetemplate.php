861028c121f726162548e1d4689392f777c70198
Michiel Scholten <michiel@aquariusoft.org>
2008-01-21 21:51:19 +0000

    Removed the forum link from the site nav of pagetemplate.php
    --HG--
    extra : convert_revision : svn%3A6d5ff042-b014-0410-9faa-87b5a50638ce/trunk%4061

--- site/pagetemplate.php
+++ site/pagetemplate.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * v0.1.06 2008-01-10
+ * v0.1.07 2008-01-10
  */
 
 /*
@@ -24,7 +24,7 @@ function buildPage($skel, $page_title, $navbar, $subnavbar, $body)
 	 * simple banner:
 	 $template .= "<div id=\"banner\"><a href=\"" . $skel["base_uri"] . "\"><img src=\"" . $skel["base_uri"] . "images/aquariusoft_org.png\" alt=\"aquariusoft.org logo\" width=\"170\" height=\"53\" /></a></div>\n";
 	 */
-	$template .= "<div id=\"banner\"><span class=\"logo\"><a href=\"" . $skel["base_uri"] . "\"><img src=\"" . $skel["base_uri"] . "images/aquariusoft_org.png\" alt=\"aquariusoft.org\" width=\"170\" height=\"53\" /></a></span><span class=\"portalnav\"><a href=\"http://aquariusoft.org/gallery/\">gallery</a> | <a href=\"http://aquariusoft.org/bugs/\">bugtracker</a> | <a href=\"http://aquariusoft.org/forum/\">forum</a> | <a href=\"http://aquariusoft.org/~mbscholt/\">dammIT weblog</a> | <a href=\"http://aquariusoft.org/photolog/\">photolog</a> | <a href=\"http://jakerockwell.aquariusoft.org/\">jake rockwell</a> | <a href=\"http://www.cs.vu.nl/~mbscholt/\">:M</a> | <a href=\"/page/main/about/\">about</a></span></div>\n";
+	$template .= "<div id=\"banner\"><span class=\"logo\"><a href=\"" . $skel["base_uri"] . "\"><img src=\"" . $skel["base_uri"] . "images/aquariusoft_org.png\" alt=\"aquariusoft.org\" width=\"170\" height=\"53\" /></a></span><span class=\"portalnav\"><a href=\"http://aquariusoft.org/gallery/\">gallery</a> | <a href=\"http://aquariusoft.org/bugs/\">bugtracker</a> | <a href=\"http://aquariusoft.org/~mbscholt/\">dammIT weblog</a> | <a href=\"http://aquariusoft.org/photolog/\">photolog</a> | <a href=\"http://jakerockwell.aquariusoft.org/\">jake rockwell</a> | <a href=\"http://www.cs.vu.nl/~mbscholt/\">:M</a> | <a href=\"/page/main/about/\">about</a></span></div>\n";
 
 	/* Hack for IE's whitespace bug caused by the float */
 	$template .= "<br style=\"clear:both; height: 1px;\" />\n";
