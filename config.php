8e5c334a15d7eb90aaed8ae707457e3d56b007fd
Michiel Scholten <michiel@aquariusoft.org>
2010-01-29 15:59:28 +0000

    20100124-25: lots and lots of style changes, fixes and content cleanups
    --HG--
    extra : convert_revision : svn%3A6d5ff042-b014-0410-9faa-87b5a50638ce/trunk%40139

--- config.php
+++ config.php
@@ -23,7 +23,8 @@ $skel['404_message'] = '<p>If you are looking for the overload feedreader, pleas
 $skel['404_message'] .= '<p><em>The gallery will be up soon again</em>; as fast as the old server arrives here and the files have been copied.</p>';
 */
 //$skel['404_message'] .= '<p>Also, the bugtracker, gallery, forum and webmail and the weblog dammIT will be online again soon.</p>';
-$skel['404_message'] = '<p>If you are looking for the overload feedreader, please note <a href="page/html/overload">it has been discontinued</a>. It might come back, rewritten in Python/Django, for example, but not soon.</p>';
+$skel['404_message'] = '<p>If you are looking for the overload feedreader, please note <a href="page/html/overload">it has been discontinued</a>. It might come back -- rewritten in Python/Django for example -- but not soon.</p>';
+$skel['404_message'] .= '<p>The Windows section is gone, as the applications were unmaintained and quite antique.</p>';
 $skel['404_message'] .= '<p>Also, the forum has been discontinued because of disuse.</p>';
 $skel['404_message'] .= '<p>If you notice something missing, don\'t hesitate to <a href="page/main/contact/">contact me</a>.</p>';
 
